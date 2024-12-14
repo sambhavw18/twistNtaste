6311614ac974994f6cfd54df609ce9ddb453dfc3
sambhavw18 <sambhavwadhwa123@gmail.com>
2024-12-14 11:45:05 +0530

    newfiles

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function toggleMenu() {
+    const links = document.querySelector('.navbar-links');
+    links.classList.toggle('active');
+}
+
+// slider
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.remove('active');
+            if (i === index) {
+                slide.classList.add('active');
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    // Show the first slide initially
+    showSlide(currentSlide);
+
+    // Auto-slide every 2 seconds
+    setInterval(nextSlide, 3000);
+
+// email
